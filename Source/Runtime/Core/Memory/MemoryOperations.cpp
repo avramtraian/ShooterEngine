2db4cea704bc0469c21a606392e648311c66c30e
Avram Traian <traian.avram1@gmail.com>
2024-05-04 21:55:41 +0300

    Runtime: Fixed a typo that caused the copy_memory function to misbehave

--- Source/Runtime/Core/Memory/MemoryOperations.cpp
+++ Source/Runtime/Core/Memory/MemoryOperations.cpp
@@ -10,7 +10,7 @@ namespace SE {
 SHOOTER_API void copy_memory(void* destination, const void* source, usize byte_count)
 {
     WriteonlyBytes dst_bytes = reinterpret_cast<WriteonlyBytes>(destination);
-    ReadonlyBytes src_bytes = reinterpret_cast<ReadonlyBytes>(destination);
+    ReadonlyBytes src_bytes = reinterpret_cast<ReadonlyBytes>(source);
 
     for (usize byte_offset = 0; byte_offset < byte_count; ++byte_offset)
         dst_bytes[byte_offset] = src_bytes[byte_offset];
