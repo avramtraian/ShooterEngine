2d318adf6d76765cb2da003e7a74313fdc312b37
Avram Traian <traian.avram1@gmail.com>
2024-05-05 20:06:47 +0300

    Runtime: Implemented an abstractions over the Windows file reading API

--- Source/Runtime/Core/FileSystem/FileSystem.h
+++ Source/Runtime/Core/FileSystem/FileSystem.h
@@ -11,6 +11,83 @@
 
 namespace SE {
 
+enum class FileError : u32 {
+    Success = 0,
+    Unknown = 1,
+
+    FileHandleNotOpened,
+    FileNotFound,
+    FileAlreadyInUse,
+    PermissionDenied,
+    ReadOutOfBounds,
+};
+
+//
+// Abstraction over the platform API that allows reading from disk files.
+//
+class FileReader {
+public:
+    enum class OpenPolicy : u8 {
+        // Only opens the file is it exists on disk. Otherwise, the operation is considered failed.
+        OpenExisting,
+
+        // If the file doesn't exist on disk, consider it as being empty.
+        NonExistingFileIsEmpty,
+    };
+
+    enum class SharePolicy : u8 {
+        // This file can't be open by any other process.
+        Exclusive,
+        // Other processes can only open this file for reading.
+        ReadOnly,
+        // Other processes can open this file with full read/write access.
+        ReadWrite,
+    };
+
+public:
+    SHOOTER_API FileReader();
+    SHOOTER_API ~FileReader();
+
+    SHOOTER_API FileError open(const String& filepath, OpenPolicy open_policy = OpenPolicy::OpenExisting, SharePolicy share_policy = SharePolicy::Exclusive);
+    SHOOTER_API void close();
+    ALWAYS_INLINE bool is_opened() const { return m_handle_is_opened; }
+    
+    //
+    // Reads the entire file and stores its contents in a newly allocated buffer.
+    // It is the responsibility of the caller to manage the lifetime of the provided buffer.
+    // The provided buffer object must be empty, otherwise an assert will be triggered.
+    //
+    SHOOTER_API FileError read_entire(Buffer& out_buffer);
+
+    //
+    // Tries to read the entire file and store its contents in the provided buffer. If the provided
+    // buffer is not large enough, nothing will be written. In this case, the number of read bytes parameter
+    // will be set to zero and FileError::Success will be returned (if there are no other file IO errors).
+    // If an error occurs while reading the file, the number of read bytes parameter will be set to 'invalid_size'.
+    //
+    SHOOTER_API FileError try_read_entire(WriteonlyByteSpan output_buffer, usize& out_number_of_read_bytes);
+    
+    //
+    // Reads the from the file and stores its contents in a newly allocated buffer.
+    // It is the responsibility of the caller to manage the lifetime of the provided buffer.
+    // The provided buffer object must be empty, otherwise an assert will be triggered.
+    //
+    SHOOTER_API FileError read(Buffer& out_buffer, usize read_offset_in_bytes, usize number_of_bytes_to_read);
+    
+    //
+    // Tries to read from the file and store the contents in the provided buffer. If the provided
+    // buffer is not large enough, nothing will be written. In this case, the number of read bytes parameter
+    // will be set to zero and FileError::Success will be returned (if there are no other file IO errors).
+    // If an error occurs while reading the file, the number of read bytes parameter will be set to 'invalid_size'.
+    //
+    SHOOTER_API FileError try_read(WriteonlyByteSpan output_buffer, usize read_offset_in_bytes, usize number_of_bytes_to_read, usize& out_number_of_read_bytes);
+    
+private:
+    void* m_native_handle;
+    bool m_handle_is_opened;
+    OpenPolicy m_open_policy;
+};
+
 class FileSystem {
 public:
     SHOOTER_API NODISCARD static bool file_exists(const String& filepath);
--- Source/Runtime/Core/Platform/Windows/WindowsFileSystem.cpp
+++ Source/Runtime/Core/Platform/Windows/WindowsFileSystem.cpp
@@ -10,6 +10,217 @@
 namespace SE {
 
 //==============================================================================================================
+// UTILITIES.
+//==============================================================================================================
+
+ALWAYS_INLINE static LPSTR filepath_to_cstr(const String& filepath)
+{
+    return filepath.byte_span_with_null_termination().as<CHAR>().elements();
+}
+
+static FileError read_from_file(HANDLE file_handle, WriteonlyByteSpan output_buffer, usize read_offset_in_bytes, usize number_of_bytes_to_read)
+{
+    OVERLAPPED overlapped = {};
+    constexpr DWORD dword_mask = static_cast<DWORD>(-1);
+    overlapped.Offset = read_offset_in_bytes & dword_mask;
+    overlapped.OffsetHigh = read_offset_in_bytes >> (8 * sizeof(DWORD));
+
+    usize byte_offset = 0;
+    while (byte_offset < number_of_bytes_to_read)
+    {
+        constexpr usize max_bytes_to_read = static_cast<DWORD>(-1);
+        const usize bytes_to_read = Math::min(number_of_bytes_to_read - byte_offset, max_bytes_to_read);
+
+        DWORD bytes_read;
+        BOOL read_success = ReadFile(file_handle, output_buffer.elements() + byte_offset, (DWORD)(bytes_to_read), &bytes_read, &overlapped);
+        if (!read_success || bytes_read != bytes_to_read)
+            return FileError::Unknown;
+
+        byte_offset += bytes_read;
+    }
+
+    SE_ASSERT(byte_offset == number_of_bytes_to_read);
+    return FileError::Success;
+}
+
+//==============================================================================================================
+// FILE READER.
+//==============================================================================================================
+
+FileReader::FileReader()
+    : m_native_handle(INVALID_HANDLE_VALUE)
+    , m_handle_is_opened(false)
+    , m_open_policy(OpenPolicy::OpenExisting)
+{}
+
+FileReader::~FileReader()
+{
+    close();
+}
+
+FileError FileReader::open(const String& filepath, OpenPolicy open_policy /*= OpenPolicy::OpenExisting*/, SharePolicy share_policy /*= SharePolicy::Exclusive*/)
+{
+    // Close the previously opened file handle.
+    close();
+
+    m_open_policy = open_policy;
+    DWORD share_mode = 0;
+
+    switch (share_policy)
+    {
+        case SharePolicy::Exclusive: share_mode = 0; break;
+        case SharePolicy::ReadOnly:  share_mode = FILE_SHARE_READ; break;
+        case SharePolicy::ReadWrite: share_mode = FILE_SHARE_READ | FILE_SHARE_WRITE; break;
+    }
+
+    m_native_handle = CreateFileA(filepath_to_cstr(filepath), GENERIC_READ, share_mode, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+    if (m_native_handle == INVALID_HANDLE_VALUE && m_open_policy != OpenPolicy::NonExistingFileIsEmpty)
+        return FileError::FileNotFound;
+    
+    m_handle_is_opened = true;
+    return FileError::Success;
+}
+
+void FileReader::close()
+{
+    if (m_native_handle == INVALID_HANDLE_VALUE)
+        return;
+
+    CloseHandle(m_native_handle);
+    m_native_handle = INVALID_HANDLE_VALUE;
+    m_handle_is_opened = false;
+}
+
+FileError FileReader::read_entire(Buffer& out_buffer)
+{
+    SE_ASSERT(out_buffer.is_empty());
+
+    // Ensure that the file stream is ready to read.
+    if (!m_handle_is_opened)
+        return FileError::FileHandleNotOpened;
+
+    if (m_native_handle == INVALID_HANDLE_VALUE && m_open_policy == OpenPolicy::NonExistingFileIsEmpty)
+        return FileError::Success;
+
+    // Get the size of the file.
+    LARGE_INTEGER file_size_large_integer;
+    if (!GetFileSizeEx(m_native_handle, &file_size_large_integer))
+        return FileError::Unknown;
+    const usize file_size = file_size_large_integer.QuadPart;
+
+    // Allocate memory for the file contents.
+    out_buffer.allocate(file_size);
+
+    // Read from the file.
+    FileError file_error = read_from_file(m_native_handle, out_buffer.span(), 0, file_size);
+    if (file_error != FileError::Success)
+    {
+        out_buffer.release();
+        return file_error;
+    }
+
+    return FileError::Success;
+}
+
+FileError FileReader::try_read_entire(WriteonlyByteSpan output_buffer, usize& out_number_of_read_bytes)
+{
+    out_number_of_read_bytes = invalid_size;
+
+    // Ensure that the file stream is ready to read.
+    if (!m_handle_is_opened)
+        return FileError::FileHandleNotOpened;
+
+    if (m_native_handle == INVALID_HANDLE_VALUE && m_open_policy == OpenPolicy::NonExistingFileIsEmpty)
+        return FileError::Success;
+
+    // Get the size of the file.
+    LARGE_INTEGER file_size_large_integer;
+    if (!GetFileSizeEx(m_native_handle, &file_size_large_integer))
+        return FileError::Unknown;
+    const usize file_size = file_size_large_integer.QuadPart;
+
+    // Check if the provided output buffer is large enough.
+    if (output_buffer.count() < file_size)
+    {
+        out_number_of_read_bytes = 0;
+        return FileError::Success;
+    }
+
+    // Read from the file.
+    FileError file_error = read_from_file(m_native_handle, output_buffer, 0, file_size);
+    if (file_error != FileError::Success)
+        return file_error;
+
+    out_number_of_read_bytes = file_size;
+    return FileError::Success;
+}
+
+FileError FileReader::read(Buffer& out_buffer, usize read_offset_in_bytes, usize number_of_bytes_to_read)
+{
+    SE_ASSERT(out_buffer.is_empty());
+
+    // Ensure that the file stream is ready to read.
+    if (!m_handle_is_opened)
+        return FileError::FileHandleNotOpened;
+
+    if (m_native_handle == INVALID_HANDLE_VALUE && m_open_policy == OpenPolicy::NonExistingFileIsEmpty)
+        return FileError::Success;
+
+    // Get the size of the file.
+    LARGE_INTEGER file_size_large_integer;
+    if (!GetFileSizeEx(m_native_handle, &file_size_large_integer))
+        return FileError::Unknown;
+    const usize file_size = file_size_large_integer.QuadPart;
+
+    // Ensure that the reading regions is between the file bounds.
+    if (read_offset_in_bytes + number_of_bytes_to_read > file_size)
+        return FileError::ReadOutOfBounds;
+
+    // Allocate memory for the file contents.
+    out_buffer.allocate(number_of_bytes_to_read);
+
+    // Read from the file.
+    FileError file_error = read_from_file(m_native_handle, out_buffer.span(), read_offset_in_bytes, number_of_bytes_to_read);
+    if (file_error != FileError::Success)
+    {
+        out_buffer.release();
+        return file_error;
+    }
+
+    return FileError::Success;
+}
+
+FileError FileReader::try_read(WriteonlyByteSpan output_buffer, usize read_offset_in_bytes, usize number_of_bytes_to_read, usize& out_number_of_read_bytes)
+{
+    out_number_of_read_bytes = invalid_size;
+
+    // Ensure that the file stream is ready to read.
+    if (!m_handle_is_opened)
+        return FileError::FileHandleNotOpened;
+
+    if (m_native_handle == INVALID_HANDLE_VALUE && m_open_policy == OpenPolicy::NonExistingFileIsEmpty)
+        return FileError::Success;
+
+    // Get the size of the file.
+    LARGE_INTEGER file_size_large_integer;
+    if (!GetFileSizeEx(m_native_handle, &file_size_large_integer))
+        return FileError::Unknown;
+    const usize file_size = file_size_large_integer.QuadPart;
+
+    // Ensure that the reading regions is between the file bounds.
+    if (read_offset_in_bytes + number_of_bytes_to_read > file_size)
+        return FileError::ReadOutOfBounds;
+
+    // Read from the file.
+    FileError file_error = read_from_file(m_native_handle, output_buffer, read_offset_in_bytes, number_of_bytes_to_read);
+    if (file_error != FileError::Success)
+        return file_error;
+
+    out_number_of_read_bytes = number_of_bytes_to_read;
+    return FileError::Success;
+}
+
+//==============================================================================================================
 // FILE SYSTEM.
 //==============================================================================================================
 
