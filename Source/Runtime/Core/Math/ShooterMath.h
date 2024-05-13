516a0205e641fdd033178df1e5b450a881eda6d4
Avram Traian <traian.avram1@gmail.com>
2024-05-13 14:29:33 +0300

    Runtime: Added a header files that pulls all engine math includes

---
+++ Source/Runtime/Core/Math/ShooterMath.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2024 Traian Avram. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0.
+ */
+
+#pragma once
+
+#include "Core/Math/Math.h"
+
+#include "Core/Math/IntVector.h"
+#include "Core/Math/Vector2.h"
+#include "Core/Math/Vector3.h"
+#include "Core/Math/Vector4.h"
+
+#include "Core/Math/Matrix4.h"
+#include "Core/Math/Matrix4.inl"
+
+#include "Core/Math/Quaternion.h"
