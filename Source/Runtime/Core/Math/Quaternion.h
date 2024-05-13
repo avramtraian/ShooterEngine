d2a373f542071e4bf7a8c58fbb78393215e23c29
Avram Traian <traian.avram1@gmail.com>
2024-05-13 14:28:54 +0300

    Runtime: Added a math quaternion structure

---
+++ Source/Runtime/Core/Math/Quaternion.h
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2024 Traian Avram. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0.
+ */
+
+#pragma once
+
+namespace SE
+{
+namespace Internal
+{
+
+template<typename T>
+struct Quaternion
+{
+public:
+    /** These values represent the values in the standard quaternion notation. */
+    T a, b, c, d;
+};
+
+} // namespace Internal
+
+using Quaternion = Internal::Quaternion<float>;
+using Quaterniond = Internal::Quaternion<double>;
+
+} // namespace SE
