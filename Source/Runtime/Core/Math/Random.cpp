9a007e0a37e1f2f11c2a828e3256cb4afc190307
Avram Traian <traian.avram1@gmail.com>
2024-05-18 01:50:14 +0300

    Runtime: Implemented support for pseudo-random number generation

---
+++ Source/Runtime/Core/Math/Random.cpp
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2024 Traian Avram. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0.
+ */
+
+#include "Core/Math/Random.h"
+
+#include <random>
+#include <cstdint>
+
+namespace SE
+{
+
+static std::random_device s_random_device;
+
+static std::mt19937 s_random_generator_32(s_random_device());
+static std::mt19937_64 s_random_generator_64(s_random_device());
+
+static std::uniform_int_distribution<u32> s_uint_distribution_32;
+static std::uniform_int_distribution<u64> s_uint_distribution_64;
+
+u32 Random::UInt32()
+{
+    return s_uint_distribution_32(s_random_generator_32);
+}
+
+u64 Random::UInt64()
+{
+    return s_uint_distribution_64(s_random_generator_64);
+}
+
+u32 Random::UInt32Range(u32 range_min, u32 range_max)
+{
+    SE_ASSERT_DEBUG(range_min <= range_max);
+    const u32 random_value = Random::UInt32();
+    return range_min + random_value % (range_max - range_min + 1);
+}
+
+u64 Random::UInt64Range(u64 range_min, u64 range_max)
+{
+    SE_ASSERT_DEBUG(range_min <= range_max);
+    const u64 random_value = Random::UInt64();
+    return range_min + random_value % (range_max - range_min + 1);
+}
+
+float Random::FloatRange(float range_min, float range_max)
+{
+    SE_ASSERT_DEBUG(range_min <= range_max);
+    const float scalar = (float)(Random::UInt64()) / (float)(UINT64_MAX);
+    return range_min + scalar * (range_max - range_min);
+}
+
+} // namespace SE
---
+++ Source/Runtime/Core/Math/Random.h
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2024 Traian Avram. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0.
+ */
+
+#pragma once
+
+#include "Core/CoreTypes.h"
+#include "Core/EngineAPI.h"
+
+namespace SE
+{
+
+class Random
+{
+public:
+    NODISCARD SHOOTER_API static u32 UInt32();
+    NODISCARD SHOOTER_API static u64 UInt64();
+
+    NODISCARD SHOOTER_API static u32 UInt32Range(u32 range_min, u32 range_max);
+    NODISCARD SHOOTER_API static u64 UInt64Range(u64 range_min, u64 range_max);
+    NODISCARD SHOOTER_API static float FloatRange(float range_min, float range_max);
+};
+
+} // namespace SE
