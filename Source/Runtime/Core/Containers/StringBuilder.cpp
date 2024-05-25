9e9618cd1b21bf74fbb01cd3db56e543850f39fa
Avram Traian <traian.avram1@gmail.com>
2024-05-25 22:46:20 +0300

    Runtime: Fixed path delimitator insertions in StringBuilder::path_join
    Bugs were found when providing empty string view(s) to the function! ^^

--- Source/Runtime/Core/Containers/StringBuilder.cpp
+++ Source/Runtime/Core/Containers/StringBuilder.cpp
@@ -39,16 +39,19 @@ String StringBuilder::join(std::initializer_list<StringView> views_list)
 String StringBuilder::path_join(std::initializer_list<StringView> paths_list)
 {
     usize result_byte_count = 0;
+    bool last_character_is_path_delimitator = true;
+
     for (usize path_index = 0; path_index < paths_list.size(); ++path_index)
     {
         const StringView& path = paths_list.begin()[path_index];
         if (path.is_empty())
             continue;
 
-        result_byte_count += path.byte_span().count();
-
-        if (path_index != 0 && path.byte_span().first() != SE_FILEPATH_DELIMITATOR)
+        if (path_index != 0 && !last_character_is_path_delimitator && path.byte_span().first() != SE_FILEPATH_DELIMITATOR)
             result_byte_count += sizeof(SE_FILEPATH_DELIMITATOR);
+        last_character_is_path_delimitator = (path.byte_span().last() == SE_FILEPATH_DELIMITATOR);
+
+        result_byte_count += path.byte_span().count();
     }
     result_byte_count += sizeof('\0');
 
@@ -63,17 +66,20 @@ String StringBuilder::path_join(std::initializer_list<StringView> paths_list)
     }
 
     usize byte_offset = 0;
+    last_character_is_path_delimitator = true;
+
     for (usize path_index = 0; path_index < paths_list.size(); ++path_index)
     {
         const StringView& path = paths_list.begin()[path_index];
         if (path.is_empty())
             continue;
 
-        if (path_index != 0 && path.byte_span().first() != SE_FILEPATH_DELIMITATOR)
+        if (path_index != 0 && !last_character_is_path_delimitator && path.byte_span().first() != SE_FILEPATH_DELIMITATOR)
         {
             destination_buffer[byte_offset] = SE_FILEPATH_DELIMITATOR;
             byte_offset += sizeof(SE_FILEPATH_DELIMITATOR);
         }
+        last_character_is_path_delimitator = (path.byte_span().last() == SE_FILEPATH_DELIMITATOR);
 
         copy_memory_from_span(destination_buffer + byte_offset, path.byte_span());
         byte_offset += path.byte_span().count();
