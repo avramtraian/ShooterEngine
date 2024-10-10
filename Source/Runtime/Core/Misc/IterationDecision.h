34b79328049196c8b7cf2a700eafcfa386a0650d
Avram Traian <traian.avram1@gmail.com>
2024-10-10 17:51:58 +0300

    Runtime: Added an `IterationDecision` utility macro

---
+++ Source/Runtime/Core/Misc/IterationDecision.h
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2024 Traian Avram. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0.
+ */
+
+#pragma once
+
+#include <Core/CoreTypes.h>
+
+namespace SE
+{
+
+enum class IterationDecision : u8
+{
+	Continue,
+	Break,
+};
+
+} // namespace SE
