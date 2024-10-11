b1453069135898cf2c4cd73642dea0d16218337c
Avram Traian <traian.avram1@gmail.com>
2024-10-11 15:25:41 +0300

    Runtime: Reworked the `CoreDefines.h` and `CoreTypes.h` files

--- Source/Runtime/Core/CoreDefines.h
+++ Source/Runtime/Core/CoreDefines.h
@@ -5,21 +5,29 @@
 
 #pragma once
 
+//======================================================================================
+// PLATFORM CONFIGURATION MACROS.
+//======================================================================================
+//
+// NOTE: The platform and configurations macros are set by the build system.
+// It is the responsability of the build system to ensure that they are set correctly.
+//
+
 #ifndef SE_PLATFORM_WINDOWS
     #define SE_PLATFORM_WINDOWS 0
 #endif // SE_PLATFORM_WINDOWS
 
-#ifndef SE_PLATFORM_MACOS
-    #define SE_PLATFORM_MACOS 0
-#endif // SE_PLATFORM_MACOS
-
-#ifndef SE_PLATFORM_LINUX
-    #define SE_PLATFORM_LINUX 0
-#endif // SE_PLATFORM_LINUX
-
-#if !SE_PLATFORM_WINDOWS && !SE_PLATFORM_MACOS && !SE_PLATFORM_LINUX
+//
+// Ensure that at least one platform macro is set to 1.
+// Otherwise, the project configuration is wrong and a compiler error should be raised.
+//
+#if !SE_PLATFORM_WINDOWS
     #error Unknown or unsupported platform!
-#endif // No known platform.
+#endif // Any supported platform.
+
+//======================================================================================
+// BUILD CONFIGURATION MACROS.
+//======================================================================================
 
 #ifndef SE_CONFIGURATION_DEBUG
     #define SE_CONFIGURATION_DEBUG 0
@@ -33,37 +41,84 @@
     #define SE_CONFIGURATION_SHIPPING 0
 #endif // SE_CONFIGURATION_SHIPPING
 
+//
+// Ensure that at least one build configuration macro is set to 1.
+// Otherwise, the project configuration is wrong and a compiler error should be raised.
+//
 #if !SE_CONFIGURATION_DEBUG && !SE_CONFIGURATION_DEVELOPMENT && !SE_CONFIGURATION_SHIPPING
-    #error No build configuration was specified!
-#endif // No configuration specified.
+    #error Unknown or unsupported build configuration!
+#endif // Any supported build configuration.
 
-#ifdef _MSC_BUILD
-    #define SE_COMPILER_MSVC  1
-    #define SE_COMPILER_GCC   0
-    #define SE_COMPILER_CLANG 0
+//======================================================================================
+// COMPILER CONFIGURATION MACROS.
+//======================================================================================
+
+// The `__clang__` is only set when compiling using clang.
+#ifdef __clang__
+    #define SE_COMPILER_CLANG 1
 #else
-    #define SE_COMPILER_MSVC  0
-    #define SE_COMPILER_GCC   0
+    // The `_MSC_BUILD` is defined when compiling with MSVC, or sometimes when using clang. As we
+    // already checked for the clang compiler, we can be confident that this flag is only set when
+    // using MSVC.
+    #ifdef _MSC_BUILD
+        #define SE_COMPILER_MSVC 1
+    #endif // _MSC_BUILD
+
+    // Similarly to the `_MSC_BUILD` flag, `__gnuc__` is only set when compiling using GCC.
+    #ifdef __GNUC__
+        #define SE_COMPILER_GCC 1
+    #endif // __GNUC__
+#endif // __clang__
+
+#ifndef SE_COMPILER_MSVC
+    #define SE_COMPILER_MSVC 0
+#endif // SE_COMPILER_MSVC
+
+#ifndef SE_COMPILER_CLANG
     #define SE_COMPILER_CLANG 0
-#endif // _MSC_BUILD
+#endif // SE_COMPILER_CLANG
 
-#if !SE_COMPILER_MSVC
+#ifndef SE_COMPILER_GCC
+    #define SE_COMPILER_GCC 0
+#endif // SE_COMPILER_GCC
+
+//
+// Ensure that at least one compiler macro is set to 1.
+// Otherwise, an unknown or unsupported compiler is used, which should raise a compiler error.
+//
+#if !SE_COMPILER_MSVC && !SE_COMPILER_CLANG && !SE_COMPILER_GCC
     #error Unknown or unsupported compiler!
-#endif // No known compiler.
+#endif // Any supported compiler.
 
-#define SE_LINE __LINE__
-#define SE_FILE __FILE__
-#define SE_DATE __DATE__
-
-#define NODISCARD    [[nodiscard]]
-#define MAYBE_UNUSED [[maybe_unused]]
-#define LIKELY       [[likely]]
-#define UNLIKELY     [[unlikely]]
-
-#define SE_ARRAY_COUNT(x) (sizeof(x) / sizeof((x)[0]))
+//======================================================================================
+// UTILITY (GENERAL PURPOSE) MACROS.
+//======================================================================================
 
 #if SE_COMPILER_MSVC
-    #define SE_FUNCTION   __FUNCSIG__
-    #define SE_DEBUGBREAK __debugbreak()
+    // Hint for the compiler that the function should always be inlined.
     #define ALWAYS_INLINE __forceinline
-#endif // Compiler switch.
+
+    // Traps the debugger. Triggers a breakpoint if a debugger is attached or crashes the program otherwise.
+    #define SE_DEBUGBREAK __debugbreak()
+
+    // Expands to the signature of the function in which the macro is located.
+    #define SE_FUNCTION __FUNCSIG__
+#endif // SE_COMPILER_MSVC
+
+// The compiler is encouraged to issue a warning if the function return value is not stored/used.
+#define NODISCARD [[nodiscard]]
+
+// Suppreses warnings on unused entities.
+#define MAYBE_UNUSED [[maybe_unused]]
+
+// Represent hints to the compiler that the path of execution is more or less likely than the alternative.
+#define LIKELY   [[likely]]
+#define UNLIKELY [[unlikely]]
+
+// Expands to the number of elements stored in the given static array.
+#define SE_ARRAY_COUNT(x) (sizeof(x) / sizeof((x)[0]))
+
+// Constants regarding memory size units.
+#define KiB (static_cast<usize>(1024))
+#define MiB (1024 * KiB)
+#define GiB (1024 * MiB)
--- Source/Runtime/Core/CoreTypes.h
+++ Source/Runtime/Core/CoreTypes.h
@@ -5,30 +5,45 @@
 
 #pragma once
 
-#include "Core/CoreDefines.h"
-
-namespace SE {
+#include <Core/CoreDefines.h>
+#include <type_traits>
 
 #if SE_PLATFORM_WINDOWS
+namespace SE
+{
 
+//
+// Fixed-size primitive types that represent an unsigned integer.
+// Their sizes can always be assumed as they are guaranteed to be the same on all platforms.
+//
 using u8 = unsigned char;
 using u16 = unsigned short;
 using u32 = unsigned int;
 using u64 = unsigned long long;
 
+//
+// Fixed-size primitive types that represent an signed integer.
+// Their sizes can always be assumed as they are guaranteed to be the same on all platforms.
+//
 using i8 = signed char;
 using i16 = signed short;
 using i32 = signed int;
 using i64 = signed long long;
 
-using usize = unsigned long long;
-using ssize = signed long long;
-using uintptr = unsigned long long;
+//
+// Primitive types that represent integers which hold a size or memory address.
+// Never assume their sizes, as they are not guaranteed to be the same on all platforms.
+//
+using usize = u64;
+using ssize = i64;
+using uintptr = u64;
+using intptr = i64;
 
-using f32 = float;
-using f64 = double;
+} // namespace SE
+#endif // SE_PLATFORM_WINDOWS
 
-#endif // Platform switch.
+namespace SE
+{
 
 using ReadonlyByte = const u8;
 using WriteonlyByte = u8;
@@ -43,139 +58,114 @@ using UnicodeCodepoint = u32;
 constexpr usize invalid_size = static_cast<usize>(-1);
 constexpr UnicodeCodepoint invalid_unicode_codepoint = static_cast<UnicodeCodepoint>(-1);
 
-namespace Internal {
+} // namespace SE
 
-template<typename T>
-struct RemoveReference {
-    using Type = T;
-};
-template<typename T>
-struct RemoveReference<T&> {
-    using Type = T;
-};
-template<typename T>
-struct RemoveReference<T&&> {
-    using Type = T;
-};
-
-template<typename T>
-struct RemoveConst {
-    using Type = T;
-};
-template<typename T>
-struct RemoveConst<const T> {
-    using Type = T;
-};
-
-template<typename T>
-struct IsIntegral {
-    static constexpr bool value = false;
-    static constexpr bool is_signed = false;
-};
-
-template<>
-struct IsIntegral<u8> {
-    static constexpr bool value = true;
-    static constexpr bool is_signed = false;
-};
-template<>
-struct IsIntegral<u16> {
-    static constexpr bool value = true;
-    static constexpr bool is_signed = false;
-};
-template<>
-struct IsIntegral<u32> {
-    static constexpr bool value = true;
-    static constexpr bool is_signed = false;
-};
-template<>
-struct IsIntegral<u64> {
-    static constexpr bool value = true;
-    static constexpr bool is_signed = false;
-};
-template<>
-struct IsIntegral<i8> {
-    static constexpr bool value = true;
-    static constexpr bool is_signed = true;
-};
-template<>
-struct IsIntegral<i16> {
-    static constexpr bool value = true;
-    static constexpr bool is_signed = true;
-};
-template<>
-struct IsIntegral<i32> {
-    static constexpr bool value = true;
-    static constexpr bool is_signed = true;
-};
-template<>
-struct IsIntegral<i64> {
-    static constexpr bool value = true;
-    static constexpr bool is_signed = true;
-};
-
-} // namespace Internal
-
-template<typename T>
-using RemoveReference = typename Internal::RemoveReference<T>::Type;
-template<typename T>
-using RemoveConst = typename Internal::RemoveConst<T>::Type;
-
-template<typename T>
-constexpr bool is_integral = Internal::IsIntegral<T>::value;
-template<typename T>
-constexpr bool is_signed_integral = is_integral<T> && Internal::IsIntegral<T>::is_signed;
-template<typename T>
-constexpr bool is_unsigned_integral = is_integral<T> && !Internal::IsIntegral<T>::is_signed;
-
-template<typename T>
-NODISCARD ALWAYS_INLINE constexpr RemoveReference<T>&& move(T&& value)
-{
-    // https://en.cppreference.com/w/cpp/utility/move
-    return static_cast<RemoveReference<T>&&>(value);
-}
-
-template<typename T>
-NODISCARD ALWAYS_INLINE constexpr T&& forward(RemoveReference<T>& value)
-{
-    // https://en.cppreference.com/w/cpp/utility/forward
-    return static_cast<T&&>(value);
-}
-
-template<typename T>
-NODISCARD ALWAYS_INLINE constexpr T&& forward(RemoveReference<T>&& value)
-{
-    // https://en.cppreference.com/w/cpp/utility/forward
-    return static_cast<T&&>(value);
-}
-
+// Marks the type in which this macro is placed as non-copyable, by marking the
+// copy constructor and assignment operator as deleted.
 #define SE_MAKE_NONCOPYABLE(type_name)    \
     type_name(const type_name&) = delete; \
     type_name& operator=(const type_name&) = delete
 
+// Marks the type in which this macro is placed as non-movable, by marking the
+// move constructor and assignment operator as deleted.
 #define SE_MAKE_NONMOVABLE(type_name)         \
     type_name(type_name&&) noexcept = delete; \
     type_name& operator=(type_name&&) noexcept = delete
 
+namespace SE
+{
+
+// clang-format off
+namespace Detail
+{
+
+template<typename T> struct RemoveReference         { using Type = T; };
+template<typename T> struct RemoveReference<T&>     { using Type = T; };
+template<typename T> struct RemoveReference<T&&>    { using Type = T; };
+
+template<typename T> struct RemoveConst             { using Type = T; };
+template<typename T> struct RemoveConst<const T>    { using Type = T; };
+
+template<typename T> struct RemovePointer           { using Type = T; };
+template<typename T> struct RemovePointer<T*>       { using Type = T; };
+
+template<typename T>    struct IsIntegral      { static constexpr bool value = false;  static constexpr bool is_signed = false; };
+template<>              struct IsIntegral<u8>  { static constexpr bool value = true;   static constexpr bool is_signed = false; };
+template<>              struct IsIntegral<u16> { static constexpr bool value = true;   static constexpr bool is_signed = false; };
+template<>              struct IsIntegral<u32> { static constexpr bool value = true;   static constexpr bool is_signed = false; };
+template<>              struct IsIntegral<u64> { static constexpr bool value = true;   static constexpr bool is_signed = false; };
+template<>              struct IsIntegral<i8>  { static constexpr bool value = true;   static constexpr bool is_signed = true; };
+template<>              struct IsIntegral<i16> { static constexpr bool value = true;   static constexpr bool is_signed = true; };
+template<>              struct IsIntegral<i32> { static constexpr bool value = true;   static constexpr bool is_signed = true; };
+template<>              struct IsIntegral<i64> { static constexpr bool value = true;   static constexpr bool is_signed = true; };
+
+} // namespace Detail
+// clang-format on
+
 //
-// Ensure that the fixed primitive types are matching the expected size.
+// Remove the reference from a type.
+// For example, `const int&` becomes `const int`, while `float` remains `float`.
 //
+template<typename T>
+using RemoveReference = typename Detail::RemoveReference<T>::Type;
 
-static_assert(sizeof(u8) == 1);
-static_assert(sizeof(u16) == 2);
-static_assert(sizeof(u32) == 4);
-static_assert(sizeof(u64) == 8);
+//
+// Remove the constness from a type.
+// For example, `const int&` becomes `int&`, while `float` remains `float`.
+//
+template<typename T>
+using RemoveConst = typename Detail::RemoveConst<T>::Type;
 
-static_assert(sizeof(i8) == 1);
-static_assert(sizeof(i16) == 2);
-static_assert(sizeof(i32) == 4);
-static_assert(sizeof(i64) == 8);
+template<typename T>
+constexpr bool is_integral = Detail::IsIntegral<T>::value;
+template<typename T>
+constexpr bool is_signed_integral = is_integral<T> && Detail::IsIntegral<T>::is_signed;
+template<typename T>
+constexpr bool is_unsigned_integral = is_integral<T> && !Detail::IsIntegral<T>::is_signed;
 
-static_assert(sizeof(usize) == sizeof(void*));
-static_assert(sizeof(ssize) == sizeof(void*));
-static_assert(sizeof(uintptr) == sizeof(void*));
+// Wrapper around `std::is_base_of_v`.
+template<typename DerivedType, typename BaseType>
+constexpr bool is_derived_from = std::is_base_of_v<BaseType, DerivedType>;
 
-static_assert(sizeof(f32) == 4);
-static_assert(sizeof(f64) == 8);
+// Wrapper around `std::is_base_of_v`.
+template<typename BaseType, typename DerivedType>
+constexpr bool is_base_of = std::is_base_of_v<BaseType, DerivedType>;
+
+//
+// Remove the pointer from a type.
+// For example, `const int*` becomes `const int`, while `float` remains `float`.
+//
+template<typename T>
+using RemovePointer = typename Detail::RemovePointer<T>::Type;
+
+//
+// Used in move semantics. Follows the C++ standard signature, which can be found at:
+// https://en.cppreference.com/w/cpp/utility/move
+//
+template<typename T>
+NODISCARD ALWAYS_INLINE constexpr RemoveReference<T>&& move(T&& value)
+{
+    return static_cast<RemoveReference<T>&&>(value);
+}
+
+//
+// Used in move semantics. Follows the C++ standard signature, which can be found at:
+// https://en.cppreference.com/w/cpp/utility/forward
+//
+template<typename T>
+NODISCARD ALWAYS_INLINE constexpr T&& forward(RemoveReference<T>& value)
+{
+    return static_cast<T&&>(value);
+}
+
+//
+// Used in move semantics. Follows the C++ standard signature, which can be found at:
+// https://en.cppreference.com/w/cpp/utility/forward
+//
+template<typename T>
+NODISCARD ALWAYS_INLINE constexpr T&& forward(RemoveReference<T>&& value)
+{
+    return static_cast<T&&>(value);
+}
 
 } // namespace SE
