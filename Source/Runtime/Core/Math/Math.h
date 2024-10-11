da9a99739a76a3cce94de525c7ebd360846ec6ac
Avram Traian <traian.avram1@gmail.com>
2024-10-11 22:59:43 +0300

    Runtime: Added a math `Axis` enumeration type

--- Source/Runtime/Core/Math/Math.h
+++ Source/Runtime/Core/Math/Math.h
@@ -21,17 +21,32 @@ namespace SE
 namespace Math
 {
 
-/** PI constants. */
+//
+// PI constants.
+//
 static constexpr float pi = 3.1415926535F;
 static constexpr float two_pi = 2.0F * pi;
 static constexpr float half_pi = 0.5F * pi;
 static constexpr float one_over_pi = 1.0F / pi;
 
-/** Numerical constants that express the magnitude of numbers. */
+//
+// Numerical constants that express the magnitude of numbers.
+//
 static constexpr float small_number = 1e-3F;
 static constexpr float very_small_number = 1e-4F;
 static constexpr float large_number = 1e20F;
 static constexpr float very_large_number = 1e35F;
+
+//
+// Enum that lists all possible axis types.
+//
+enum class Axis : u8
+{
+    X = 0,
+    Y = 1,
+    Z = 2,
+    W = 3,
+};
 
 template<typename T>
 NODISCARD ALWAYS_INLINE static constexpr T abs(T value)
@@ -63,23 +78,23 @@ NODISCARD ALWAYS_INLINE static constexpr T clamp(T value, T in_min, T in_max)
     return min(max(value, in_min), in_max);
 }
 
-/** Elementary functions. */
 SHOOTER_API NODISCARD float sqrt(float value);
 SHOOTER_API NODISCARD float pow(float base, float exponent);
-/** The natural logarithm of a number. */
 SHOOTER_API NODISCARD float log(float value);
-/** The logarithm of a number in base 2. */
 SHOOTER_API NODISCARD float log2(float value);
-/** The logarithm of a number in base 10. */
 SHOOTER_API NODISCARD float log10(float value);
 
-/** Trigonometric functions. */
+//
+// Trigonometric functions.
+//
 SHOOTER_API NODISCARD float sin(float value);
 SHOOTER_API NODISCARD float cos(float value);
 SHOOTER_API NODISCARD void sin_and_cos(float value, float& out_sin, float& out_cos);
 SHOOTER_API NODISCARD float tan(float value);
 
-/** Inverses of trigonometric functions. */
+//
+// Inverses of trigonometric functions.
+//
 SHOOTER_API NODISCARD float arcsin(float value);
 SHOOTER_API NODISCARD float arccos(float value);
 SHOOTER_API NODISCARD float arctan(float value);
