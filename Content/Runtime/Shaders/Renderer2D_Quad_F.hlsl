ad65676bfa0923ccbc6efda746aa0bef94b68a0d
Avram Traian <traian.avram1@gmail.com>
2024-10-13 13:44:19 +0300

    Runtime: Fixed compiler warnings and code style in `Renderer2D_Quad_F`

--- Content/Runtime/Shaders/Renderer2D_Quad_F.hlsl
+++ Content/Runtime/Shaders/Renderer2D_Quad_F.hlsl
@@ -15,24 +15,24 @@ struct FSOutput
     float4 color : SV_Target0;
 };
 
-Texture2D   u_Textures[32]      : register(t0);
-sampler     u_TextureSampler    : register(s0);
+Texture2D u_textures[32] : register(t0);
+sampler u_texture_sampler : register(s0);
 
 FSOutput fragment_main(FSInput input)
 {
     FSOutput output;
-    float4 texel;
+    float4 texel = float4(1, 1, 1, 1);
     
     switch (input.texture_id)
     {
-        case 0: texel = u_Textures[0].Sample(u_TextureSampler, input.texture_coordinates); break;
-        case 1: texel = u_Textures[1].Sample(u_TextureSampler, input.texture_coordinates); break;
-        case 2: texel = u_Textures[2].Sample(u_TextureSampler, input.texture_coordinates); break;
-        case 3: texel = u_Textures[3].Sample(u_TextureSampler, input.texture_coordinates); break;
-        case 4: texel = u_Textures[4].Sample(u_TextureSampler, input.texture_coordinates); break;
-        case 5: texel = u_Textures[5].Sample(u_TextureSampler, input.texture_coordinates); break;
-        case 6: texel = u_Textures[6].Sample(u_TextureSampler, input.texture_coordinates); break;
-        case 7: texel = u_Textures[7].Sample(u_TextureSampler, input.texture_coordinates); break;
+        case 0: texel = u_textures[0].Sample(u_texture_sampler, input.texture_coordinates); break;
+        case 1: texel = u_textures[1].Sample(u_texture_sampler, input.texture_coordinates); break;
+        case 2: texel = u_textures[2].Sample(u_texture_sampler, input.texture_coordinates); break;
+        case 3: texel = u_textures[3].Sample(u_texture_sampler, input.texture_coordinates); break;
+        case 4: texel = u_textures[4].Sample(u_texture_sampler, input.texture_coordinates); break;
+        case 5: texel = u_textures[5].Sample(u_texture_sampler, input.texture_coordinates); break;
+        case 6: texel = u_textures[6].Sample(u_texture_sampler, input.texture_coordinates); break;
+        case 7: texel = u_textures[7].Sample(u_texture_sampler, input.texture_coordinates); break;
     }
     
     output.color = input.color * texel;
