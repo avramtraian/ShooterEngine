5ff51714cef10d72421030c77f9df96c85cfc6bc
Avram Traian <traian.avram1@gmail.com>
2024-10-20 18:47:06 +0300

    Editor: Implemented entity selection from withing the `SceneHierarchy`

--- Source/Editor/EditorContext/Panels/SceneHierarchyPanel.cpp
+++ Source/Editor/EditorContext/Panels/SceneHierarchyPanel.cpp
@@ -44,8 +44,19 @@ void SceneHierarchyPanel::on_render_imgui()
         // TODO: Actually sort the entries by name before rendering them.
         // ordered_entity_entries.sort();
 
+        bool is_any_entry_clicked = false;
         for (const EntityEntry& entry : ordered_entity_entries)
-            render_entity_entry(entry);
+        {
+            const bool entry_is_clicked = draw_entity_entry(entry);
+            is_any_entry_clicked = is_any_entry_clicked || entry_is_clicked;
+        }
+
+        if (!is_any_entry_clicked && ImGui::IsWindowHovered() && ImGui::IsMouseClicked(0))
+        {
+            // If no entity entry was hovered but the mouse was clicked the user wants to clear
+            // the selection context.
+            clear_selected_entity();
+        }
     }
 
     ImGui::End();
@@ -53,15 +64,57 @@ void SceneHierarchyPanel::on_render_imgui()
 
 void SceneHierarchyPanel::set_scene_context(Scene* scene_context)
 {
+    SE_ASSERT(scene_context != nullptr);
     m_scene_context = scene_context;
 }
 
 void SceneHierarchyPanel::clear_scene_context()
-{}
-
-void SceneHierarchyPanel::render_entity_entry(const EntityEntry& entry)
 {
-    ImGui::Text(entry.name.characters());
+    m_scene_context = nullptr;
+}
+
+bool SceneHierarchyPanel::draw_entity_entry(const EntityEntry& entry)
+{
+    bool entry_is_clicked = false;
+    ImGui::PushID(reinterpret_cast<const void*>(entry.entity.uuid().value()));
+
+    ImGuiTreeNodeFlags tree_node_flags = ImGuiTreeNodeFlags_SpanAvailWidth | ImGuiTreeNodeFlags_OpenOnArrow;
+    if (entry.entity.uuid() == m_selected_entity_uuid.value_or(UUID::invalid()))
+        tree_node_flags |= ImGuiTreeNodeFlags_Selected;
+
+    bool is_tree_node_opened = ImGui::TreeNodeEx(entry.name.characters(), tree_node_flags);
+
+    if (ImGui::IsItemClicked())
+    {
+        const UUID entity_uuid = entry.entity.uuid();
+        set_selected_entity(entity_uuid);
+        entry_is_clicked = true;
+    }
+
+    if (is_tree_node_opened)
+        ImGui::TreePop();
+
+    ImGui::PopID();
+    return entry_is_clicked;
+}
+
+void SceneHierarchyPanel::set_selected_entity(UUID selected_entity_uuid)
+{
+    SE_ASSERT(selected_entity_uuid != UUID::invalid());
+    m_selected_entity_uuid = selected_entity_uuid;
+    dispatch_on_selection_changed_callbacks();
+}
+
+void SceneHierarchyPanel::clear_selected_entity()
+{
+    m_selected_entity_uuid.clear();
+    dispatch_on_selection_changed_callbacks();
+}
+
+void SceneHierarchyPanel::dispatch_on_selection_changed_callbacks()
+{
+    for (PFN_OnSceneHierarchySelectionChanged& callback : m_on_selection_changed_callbacks)
+        callback(m_selected_entity_uuid);
 }
 
 } // namespace SE
--- Source/Editor/EditorContext/Panels/SceneHierarchyPanel.h
+++ Source/Editor/EditorContext/Panels/SceneHierarchyPanel.h
@@ -5,13 +5,30 @@
 
 #pragma once
 
+#include <Core/Containers/Function.h>
+#include <Core/Containers/Optional.h>
+#include <Core/UUID.h>
 #include <Engine/Scene/Scene.h>
 
 namespace SE
 {
 
+using PFN_OnSceneHierarchySelectionChanged = Function<void(Optional<UUID> selected_entity_uuid)>;
+
 class SceneHierarchyPanel
 {
+public:
+    struct EntityEntry
+    {
+        ALWAYS_INLINE EntityEntry(Entity& in_entity, const String& in_name)
+            : entity(in_entity)
+            , name(in_name)
+        {}
+
+        Entity& entity;
+        const String& name;
+    };
+
 public:
     bool initialize();
     void shutdown();
@@ -24,25 +41,26 @@ public:
     NODISCARD ALWAYS_INLINE const Scene* get_scene_context() const { return m_scene_context; }
 
     void set_scene_context(Scene* scene_context);
-
     void clear_scene_context();
 
-private:
-    struct EntityEntry
+    ALWAYS_INLINE void add_on_selection_changed_callback(PFN_OnSceneHierarchySelectionChanged callback)
     {
-        ALWAYS_INLINE EntityEntry(Entity& in_entity, const String& in_name)
-            : entity(in_entity)
-            , name(in_name)
-        {}
+        SE_ASSERT(callback.has_value());
+        m_on_selection_changed_callbacks.emplace(move(callback));
+    }
 
-        Entity& entity;
-        const String& name;
-    };
+private:
+    bool draw_entity_entry(const EntityEntry& entry);
 
-    void render_entity_entry(const EntityEntry& entry);
+    void set_selected_entity(UUID selected_entity_uuid);
+    void clear_selected_entity();
+    void dispatch_on_selection_changed_callbacks();
 
 private:
     Scene* m_scene_context;
+
+    Optional<UUID> m_selected_entity_uuid;
+    Vector<PFN_OnSceneHierarchySelectionChanged> m_on_selection_changed_callbacks;
 };
 
 } // namespace SE
