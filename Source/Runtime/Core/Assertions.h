663ede947a491a94ba38ca04f9ff929bf1806c24
Avram Traian <traian.avram1@gmail.com>
2024-10-21 18:46:33 +0300

    Runtime: Always enable verify asserts

--- Source/Runtime/Core/Assertions.h
+++ Source/Runtime/Core/Assertions.h
@@ -10,19 +10,16 @@
 #if SE_CONFIGURATION_DEBUG
     #define SE_ASSERTION_ENABLE_DEBUG_ASSERT 1
     #define SE_ASSERTION_ENABLE_ASSERT       1
-    #define SE_ASSERTION_ENABLE_VERIFY       1
 #endif // SE_CONFIGURATION_DEBUG
 
 #if SE_CONFIGURATION_DEVELOPMENT
     #define SE_ASSERTION_ENABLE_DEBUG_ASSERT 0
     #define SE_ASSERTION_ENABLE_ASSERT       1
-    #define SE_ASSERTION_ENABLE_VERIFY       1
 #endif // SE_CONFIGURATION_DEBUG
 
 #if SE_CONFIGURATION_SHIPPING
     #define SE_ASSERTION_ENABLE_DEBUG_ASSERT 0
     #define SE_ASSERTION_ENABLE_ASSERT       0
-    #define SE_ASSERTION_ENABLE_VERIFY       1
 #endif // SE_CONFIGURATION_DEBUG
 
 #if SE_ASSERTION_ENABLE_DEBUG_ASSERT
@@ -45,12 +42,8 @@
     #define SE_ASSERT(...) // Exclude from build.
 #endif // SE_ASSERTION_ENABLE_ASSERT
 
-#if SE_ASSERTION_ENABLE_VERIFY
-    #define SE_VERIFY(...)  \
-        if (!(__VA_ARGS__)) \
-        {                   \
-            SE_DEBUGBREAK;  \
-        }
-#else
-    #define SE_VERIFY(...) // Exclude from build.
-#endif // SE_ASSERTION_ENABLE_VERIFY
+#define SE_VERIFY(...)  \
+    if (!(__VA_ARGS__)) \
+    {                   \
+        SE_DEBUGBREAK;  \
+    }
