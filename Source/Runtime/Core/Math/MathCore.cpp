2a4ead00c6bbd723de028831eb4cbf7e560190e0
Avram Traian <traian.avram1@gmail.com>
2024-10-21 14:41:17 +0300

    Runtime: Implemented core math elementary functions

--- Source/Runtime/Core/Math/MathCore.cpp
+++ Source/Runtime/Core/Math/MathCore.cpp
@@ -2,3 +2,43 @@
  * Copyright (c) 2024 Traian Avram. All rights reserved.
  * SPDX-License-Identifier: Apache-2.0.
  */
+
+#include <Core/Math/MathCore.h>
+#include <cmath>
+
+namespace SE::Math
+{
+
+//
+// Forward the core math functions implementations to the standard library.
+// Note that in the future there might be custom implementations for certain functions.
+//
+
+// clang-format off
+
+float sqrtf(float x) { return std::sqrtf(x); }
+double sqrtd(double x) { return std::sqrt(x); }
+float sinf(float x) { return std::sinf(x); }
+double sind(double x) { return std::sin(x); }
+float cosf(float x) { return std::cosf(x); }
+double cosd(double x) { return std::cos(x); }
+
+SinAndCosResultF sin_and_cos_f(float x)
+{
+	SinAndCosResultF result;
+	result.sin = std::sinf(x);
+	result.cos = std::cosf(x);
+	return result;
+}
+
+SinAndCosResultD sin_and_cos_d(double x)
+{
+	SinAndCosResultD result;
+	result.sin = std::sin(x);
+	result.cos = std::cos(x);
+	return result;
+}
+
+// clang-format on
+
+} // namespace SE::Math
--- Source/Runtime/Core/Math/MathCore.h
+++ Source/Runtime/Core/Math/MathCore.h
@@ -5,6 +5,7 @@
 
 #pragma once
 
+#include <Core/API.h>
 #include <Core/CoreTypes.h>
 
 namespace SE::Math
@@ -35,5 +36,43 @@ NODISCARD ALWAYS_INLINE constexpr T clamp(T value, T min_value, T max_value)
 {
     return Math::max(Math::min(value, max_value), min_value);
 }
+
+// clang-format off
+
+NODISCARD SHOOTER_API float sqrtf(float x);
+NODISCARD SHOOTER_API double sqrtd(double x);
+
+NODISCARD ALWAYS_INLINE float sqrt(float x) { return Math::sqrtf(x); }
+NODISCARD ALWAYS_INLINE double sqrt(double x) { return Math::sqrtd(x); }
+
+NODISCARD SHOOTER_API float sinf(float x);
+NODISCARD SHOOTER_API double sind(double x);
+
+NODISCARD ALWAYS_INLINE float sin(float x) { return Math::sinf(x); }
+NODISCARD ALWAYS_INLINE double sin(double x) { return Math::sind(x); }
+
+NODISCARD SHOOTER_API float cosf(float x);
+NODISCARD SHOOTER_API double cosd(double x);
+
+NODISCARD ALWAYS_INLINE float cos(float x) { return Math::cosf(x); }
+NODISCARD ALWAYS_INLINE double cos(double x) { return Math::cosd(x); }
+
+template<typename T>
+struct SinAndCosResult
+{
+    T sin;
+    T cos;
+};
+
+using SinAndCosResultF = SinAndCosResult<float>;
+using SinAndCosResultD = SinAndCosResult<double>;
+
+NODISCARD SHOOTER_API SinAndCosResultF sin_and_cos_f(float x);
+NODISCARD SHOOTER_API SinAndCosResultD sin_and_cos_d(double x);
+
+NODISCARD ALWAYS_INLINE SinAndCosResultF sin_and_cos(float x) { return Math::sin_and_cos_f(x); } 
+NODISCARD ALWAYS_INLINE SinAndCosResultD sin_and_cos(double x) { return Math::sin_and_cos_d(x); } 
+
+// clang-format on
 
 } // namespace SE::Math
