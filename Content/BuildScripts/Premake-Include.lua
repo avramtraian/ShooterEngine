a7b35aa18a027e20e4e3b8a7611299dca3afd828
Avram Traian <traian.avram1@gmail.com>
2025-06-22 20:48:11 +0300

    Meta: Added the `begin_filter_configuration_development_or_shipping`

--- Content/BuildScripts/Premake-Include.lua
+++ Content/BuildScripts/Premake-Include.lua
@@ -5,11 +5,12 @@ g_binaries_directory = "%{wks.location}/Binaries/%{cfg.buildcfg}"
 g_binaries_intermediate_directory = "%{wks.location}/Intermediate/Build"
 
 -- Invokes the Premake 'filter' command with various arguments.
-function begin_filter_target_editor()             filter "configurations:EditorDebug or EditorDevelopment"             end
-function begin_filter_target_game()               filter "configurations:GameDebug or GameDevelopment or GameShipping" end
-function begin_filter_configuration_debug()       filter "configurations:EditorDebug or GameDebug"                     end
-function begin_filter_configuration_development() filter "configurations:EditorDevelopment or GameDevelopment"         end
-function begin_filter_configuration_shipping()    filter "configurations:GameShipping"                                 end
+function begin_filter_target_editor()                         filter "configurations:EditorDebug or EditorDevelopment"                     end
+function begin_filter_target_game()                           filter "configurations:GameDebug or GameDevelopment or GameShipping"         end
+function begin_filter_configuration_debug()                   filter "configurations:EditorDebug or GameDebug"                             end
+function begin_filter_configuration_development()             filter "configurations:EditorDevelopment or GameDevelopment"                 end
+function begin_filter_configuration_shipping()                filter "configurations:GameShipping"                                         end
+function begin_filter_configuration_development_or_shipping() filter "configurations:EditorDevelopment or GameDevelopment or GameShipping" end
 
 -- Invokes the Premake 'filter' without any argument, esentially clearing the filtering list.
 -- Wrapper around 'filter {}' - exists in order to provide begin/end function symmetry.
