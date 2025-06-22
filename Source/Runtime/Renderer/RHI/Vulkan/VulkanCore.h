9c9eff8301212aa2288d4772db83e1e8eb2cba42
Avram Traian <traian.avram1@gmail.com>
2025-06-22 13:36:07 +0300

    Renderer: Added a function that converts `VkFormat` members to strings

--- Source/Runtime/Renderer/RHI/Vulkan/VulkanCore.h
+++ Source/Runtime/Renderer/RHI/Vulkan/VulkanCore.h
@@ -7,6 +7,8 @@
 #include <Runtime/Core/Log.h>
 #include <Runtime/Core/Platform/PlatformCoreInclude.h>
 
+#include <string>
+
 #include <vulkan/vulkan.h>
 
 #if SE_PLATFORM_WIN64
@@ -21,3 +23,22 @@
         SE_LOG_ERROR("Vulkan expression failed!\n%s", #__VA_ARGS__);  \
         SE_ASSERT_NOT_REACHED;                                        \
     }
+
+NODISCARD FORCEINLINE static std::string VulkanFormatToString(VkFormat format)
+{
+
+    switch (format)
+    {
+#define _SE_CASE(formatName) case formatName: return #formatName;
+        _SE_CASE(VK_FORMAT_B8G8R8A8_UNORM);
+        _SE_CASE(VK_FORMAT_B8G8R8A8_SNORM);
+        _SE_CASE(VK_FORMAT_B8G8R8A8_UINT);
+        _SE_CASE(VK_FORMAT_B8G8R8A8_SINT);
+        _SE_CASE(VK_FORMAT_B8G8R8A8_SRGB);
+#undef _SE_CASE
+    }
+
+    /* TODO(Traian): Extend the switch statement above to include more image
+     * formats such this return case is never reached! */
+    return "<unstringifyable>";
+}
